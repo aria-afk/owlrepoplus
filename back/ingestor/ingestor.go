1c540b88613f64787cceb11c07e558ffc8f00b9e
aria-afk <aria.lopez.github@gmail.com>
2024-12-21 15:59:55 -0800

    de-dupe data into a local mem cache

--- back/ingestor/ingestor.go
+++ back/ingestor/ingestor.go
@@ -40,6 +40,45 @@ type IndexEntryPayload struct {
 	} `json:"body"`
 }
 
+type StoreItemEntry struct {
+	StoreName string
+	Bundle    int
+	Price     int
+	Quantity  int
+}
+
+// For now we can do de-duplication in a mem cache
+// Later on if wanted can just implement this via queries
+type Cache struct {
+	Store map[string] /*item name*/ map[string] /*timestamp*/ map[string] /*id*/ StoreItemEntry
+	sync.Mutex
+}
+
+func NewCache() Cache {
+	return Cache{
+		Store: make(map[string]map[string]map[string]StoreItemEntry),
+	}
+}
+
+func (c *Cache) write(wg *sync.WaitGroup, itemName string, timestamp string, id string, payload StoreItemEntry) {
+	c.Lock()
+	defer c.Unlock()
+	defer wg.Done()
+
+	// Init new map item if doesnt exist
+	_, exists := c.Store[itemName]
+	if !exists {
+		c.Store[itemName] = make(map[string]map[string]StoreItemEntry)
+	}
+
+	_, timestampExists := c.Store[itemName][timestamp]
+	if !timestampExists {
+		c.Store[itemName][timestamp] = make(map[string]StoreItemEntry)
+	}
+
+	c.Store[itemName][timestamp][id] = payload
+}
+
 func GetAndDecode(url string, target any) error {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -50,6 +89,7 @@ func GetAndDecode(url string, target any) error {
 }
 
 func IngestFromOwlRepo() error {
+	c := NewCache()
 	// This is the main entry point to thier search "index" (loaded on owlrepo.com/items)
 	baseUrl := "https://storage.googleapis.com/owlrepo/v1/queries/search_item_listing.json"
 	searchEntries := make([]SearchIndexEntry, 0)
@@ -67,7 +107,7 @@ func IngestFromOwlRepo() error {
 
 	for i, entry := range searchEntries {
 		// TESTING: REMOVE ME
-		if i > 10 {
+		if i > 1 {
 			break
 		}
 		url := "https://storage.googleapis.com/owlrepo/v1/uploads/" + entry.TaskId + "/slim.json"
@@ -92,8 +132,30 @@ func IngestFromOwlRepo() error {
 	close(indexEntries)
 
 	for e := range indexEntries {
-		fmt.Print(e)
+		for i, p := range e.Payload {
+			if i > 1 {
+				break
+			}
+			for _, b := range p.Body.Entries {
+				wg.Add(1)
+				sem <- 1
+				go func() {
+					defer func() { <-sem }()
+					payload := StoreItemEntry{
+						StoreName: b.StoreName,
+						Bundle:    b.Bundle,
+						Price:     b.Price,
+						Quantity:  b.Quantity,
+					}
+					c.write(&wg, p.Search.Item, p.Screenshot.Timestamp, b.Id, payload)
+				}()
+			}
+		}
 	}
+
+	wg.Wait()
+
+	fmt.Print(c.Store)
 
 	return nil
 }
