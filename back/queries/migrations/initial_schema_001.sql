a0117a77d2b861b6520baef7a3a55f202b41d0c1
aria-afk <aria.lopez.github@gmail.com>
2024-12-22 17:24:11 -0800

    update to simplified schema

--- back/queries/migrations/initial_schema_001.sql
+++ back/queries/migrations/initial_schema_001.sql
@@ -1,57 +1,34 @@
+-- TODO: Remove id not exists when done testing
+
+-- Item meta data (not time related)
 create table if not exists items (
-    id text primary key
+    id text primary key,
+    p0 int not null,
+    p25 int not null,
+    p50 int not null,
+    p75 int not null,
+    p100 int not null,
+    mean int not null,
+    std int not null,
+    nowls int not null
 );
 
--- TODO: Trigger for on insert only keep 14 most recent entries
-create table if not exists item_meta (
-    time timestamp primary key,
-    item_id text references items(id),
-    min_price float not null,
-    p25_price float not null,
-    p50_price float not null,
-    p75_price float not null,
-    max_price float not null
-);
-
--- This table is meant to service the most recent prices and locations 
--- since after 24hrs the location is stale and its nice to have a list
--- of prices since somone putting a 1 meso item messes the avg
-create table if not exists item_meta_recents (
+-- Item time table
+-- 1(items) -> Many(item_hist)
+create table if not exists item_hist (
     uuid uuid primary key default gen_random_uuid(),
-    time timestamp not null,
-    item_id text references items(id),
+    item_id text references items (id),
+    time timestamp not null
+);
+
+-- Related entries for a time series entry
+-- 1(item_hist) -> Many(item_hist_entries)
+create table if not exists item_hist_entries (
+    uuid uuid primary key default gen_random_uuid(),
+    item_hist_uuid uuid references item_hist (uuid),
     owner text not null,
     store_name text not null,
     bundle int not null,
-    price float not null,
+    price int not null,
     quantity int not null
 );
-
-create function delete_old_item_meta() returns trigger as $delete_old_items_meta$
-declare 
-r record;
-i int;
-begin
-    i := 0;
-    for r in select * from item_meta order by time desc loop
-        if (i < 14) then 
-            i = i + 1;
-        else
-            delete from item_meta where time = r.timestamp;
-        end if;
-    end loop;
-end; $delete_old_items_meta$ language plpgsql;
-
-create trigger delete_old_items_meta after insert or update on item_meta 
-for each statement execute function delete_old_item_meta();
-
-create function delete_old_item_meta_recents() returns trigger as $delete_old_item_meta_recents$
-declare 
-r record;
-begin
-    select * from item_meta_recents order by time desc limit 1 into r;
-    execute delete from item_meta_recents where time != r.time;
-end; $delete_old_item_meta_recents$ language plpgsql;
-
-create trigger delete_old_item_meta_recents after insert or update on item_meta_recents
-for each statement execute function delete_old_item_meta_recents();
